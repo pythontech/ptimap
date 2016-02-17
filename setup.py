09fc0ebd4400d47783539431553ef1c420aac1ec
Colin Hogben <colin@pythontech.co.uk>
2016-02-17 21:04:59 +0000

    Add LICENSE and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#from setuptools import setup
+from distutils.core import setup
+
+setup(name='ptimap',
+      version='1.0',
+      description='IMAP utilities',
+      url='https://github.com/pythontech/ptimap',
+      author='Colin Hogben',
+      author_email='colin@pythontech.co.uk',
+      license='MIT',
+      packages=['ptimap'],
+      scripts=['imap-util'])
